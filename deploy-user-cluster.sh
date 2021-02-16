4d37b213b92e363e9b6917115b01dd53e4607f34
Aborilov Pavel <aborilov@gmail.com>
2021-02-16 11:47:50 +0300

    add thanos and prometheus charts

--- deploy-user-cluster.sh
+++ deploy-user-cluster.sh
@@ -21,6 +21,5 @@ helm --namespace monitoring upgrade --atomic --create-namespace --install promta
 
 echo ""
 echo "Installing Prometheus"
-helm --namespace monitoring upgrade --atomic --create-namespace --install prometheus prometheus-community/prometheus \
-    --set alertmanager.enabled=false \
+helm --namespace prometheus upgrade --atomic --create-namespace --install prometheus prometheus-community/prometheus --values config/prometheus/values.yaml \
     --set server.remoteWrite[0].url=http://${GW_ADDRESS}/api/v1/receive
