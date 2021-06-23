23fc8f8569d68517f1da0a701c50215048ca3d0a
Jiacheng Xu <xjcmaxwellcjx@gmail.com>
2021-06-23 18:59:21 +0800

    try to use helm 3

--- hack/deploy-seed.sh
+++ hack/deploy-seed.sh
@@ -4,25 +4,25 @@ source ./hack/lib.sh
 
 echodate ""
 echodate "Installing Minio"
-helm --namespace ${MLA_NS} upgrade --atomic --create-namespace --install minio charts/minio --values config/minio/values.yaml
+helm3 --namespace ${MLA_NS} upgrade --atomic --create-namespace --install minio charts/minio --values config/minio/values.yaml
 
 echodate ""
 echodate "Installing Grafana"
-helm --namespace ${MLA_NS} upgrade --atomic --create-namespace --install grafana charts/grafana --values config/grafana/values.yaml
+helm3 --namespace ${MLA_NS} upgrade --atomic --create-namespace --install grafana charts/grafana --values config/grafana/values.yaml
 
 echodate ""
 echodate "Installing Cortex"
-helm dependency update charts/cortex  # need that to store memcached in charts directory
-helm --namespace ${MLA_NS} upgrade --atomic --create-namespace --install cortex charts/cortex --values config/cortex/values.yaml
+helm3 dependency update charts/cortex  # need that to store memcached in charts directory
+helm3 --namespace ${MLA_NS} upgrade --atomic --create-namespace --install cortex charts/cortex --values config/cortex/values.yaml
 
 echodate ""
 echodate "Installing Loki"
-helm --namespace ${MLA_NS} upgrade --atomic --create-namespace --install loki-distributed charts/loki-distributed --values config/loki/values.yaml
+helm3 --namespace ${MLA_NS} upgrade --atomic --create-namespace --install loki-distributed charts/loki-distributed --values config/loki/values.yaml
 
 echodate ""
 echodate "Installing Alertmanager Proxy"
-helm --namespace ${MLA_NS} upgrade --atomic --create-namespace --install alertmanager-proxy charts/alertmanager-proxy
+helm3 --namespace ${MLA_NS} upgrade --atomic --create-namespace --install alertmanager-proxy charts/alertmanager-proxy
 
 echodate ""
 echodate "Installing Minio Bucket Lifecycle Manager"
-helm --namespace ${MLA_NS} upgrade --atomic --create-namespace --install minio-lifecycle-mgr charts/minio-lifecycle-mgr --values config/minio-lifecycle-mgr/values.yaml
+helm3 --namespace ${MLA_NS} upgrade --atomic --create-namespace --install minio-lifecycle-mgr charts/minio-lifecycle-mgr --values config/minio-lifecycle-mgr/values.yaml
