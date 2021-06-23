f2f7326f36623853acc777cf5e746eafb585c2fb
Jiacheng Xu <xjcmaxwellcjx@gmail.com>
2021-06-23 18:21:59 +0800

    Add job

---
+++ hack/ci/deploy-dev.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Copyright 2020 The Kubermatic Kubernetes Platform contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+### This script is used as a postsubmit job and updates the mla stack in dev master
+### cluster after every commit to master.
+
+set -euo pipefail
+
+cd $(dirname $0)/../..
+
+echodate "Getting secrets from Vault"
+export VAULT_ADDR=https://vault.loodse.com/
+retry 5 vault write \
+  --format=json auth/approle/login \
+  role_id=${VAULT_ROLE_ID} secret_id=${VAULT_SECRET_ID} > /tmp/vault-token-response.json
+export VAULT_TOKEN="$(cat /tmp/vault-token-response.json | jq .auth.client_token -r)"
+export KUBECONFIG=/tmp/kubeconfig
+
+# deploy to dev
+vault kv get -field=kubeconfig dev/seed-clusters/dev.kubermatic.io > ${KUBECONFIG}
+kubectl config use-context asia-south1-c
+echodate "Successfully got secrets for dev from Vault"
+
+./hack/deploy-seed.sh
--- hack/deploy-seed.sh
+++ hack/deploy-seed.sh
@@ -1,29 +1,26 @@
 #!/usr/bin/env bash
 
-# MLA components namespace
-MLA_NS=mla
-
-echo ""
-echo "Installing Minio"
+echodate ""
+echodate "Installing Minio"
 helm --namespace ${MLA_NS} upgrade --atomic --create-namespace --install minio charts/minio --values config/minio/values.yaml
 
-echo ""
-echo "Installing Grafana"
+echodate ""
+echodate "Installing Grafana"
 helm --namespace ${MLA_NS} upgrade --atomic --create-namespace --install grafana charts/grafana --values config/grafana/values.yaml
 
-echo ""
-echo "Installing Cortex"
+echodate ""
+echodate "Installing Cortex"
 helm dependency update charts/cortex  # need that to store memcached in charts directory
 helm --namespace ${MLA_NS} upgrade --atomic --create-namespace --install cortex charts/cortex --values config/cortex/values.yaml
 
-echo ""
-echo "Installing Loki"
+echodate ""
+echodate "Installing Loki"
 helm --namespace ${MLA_NS} upgrade --atomic --create-namespace --install loki-distributed charts/loki-distributed --values config/loki/values.yaml
 
-echo ""
-echo "Installing Alertmanager Proxy"
+echodate ""
+echodate "Installing Alertmanager Proxy"
 helm --namespace ${MLA_NS} upgrade --atomic --create-namespace --install alertmanager-proxy charts/alertmanager-proxy
 
-echo ""
-echo "Installing Minio Bucket Lifecycle Manager"
+echodate ""
+echodate "Installing Minio Bucket Lifecycle Manager"
 helm --namespace ${MLA_NS} upgrade --atomic --create-namespace --install minio-lifecycle-mgr charts/minio-lifecycle-mgr --values config/minio-lifecycle-mgr/values.yaml
