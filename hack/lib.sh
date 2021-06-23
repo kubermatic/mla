c7abc42ad1353a598dc40ba259f7a2c0f9e74fcd
Jiacheng Xu <xjcmaxwellcjx@gmail.com>
2021-06-23 18:45:28 +0800

    add retry

--- hack/lib.sh
+++ hack/lib.sh
@@ -4,3 +4,15 @@ echodate() {
   # do not use -Is to keep this compatible with macOS
   echo "[$(date +%Y-%m-%dT%H:%M:%S%:z)]" "$@"
 }
+
+retry() {
+  # Works only with bash but doesn't fail on other shells
+  start_time=$(date +%s)
+  set +e
+  actual_retry $@
+  rc=$?
+  set -e
+  elapsed_time=$(($(date +%s) - $start_time))
+  write_junit "$rc" "$elapsed_time"
+  return $rc
+}
