efdd666a627afcedf1807ad577bfda2cbaa229f2
Wojciech Urbański <wojciech@kubermatic.com>
2022-04-14 16:44:18 +0200

    [consul] Remove old tests

--- charts/consul/test/terraform/eks/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "kubeconfigs" {
-  value = [for cl in module.eks : pathexpand(format("~/.kube/%s", cl.cluster_id))]
-}
--- charts/consul/test/terraform/openshift/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "kubeconfigs" {
-  value = [for rg in azurerm_resource_group.test : format("$HOME/.kube/%s", rg.name)]
-}
